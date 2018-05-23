3fa6f8d13c7a6d754c24e704149831f195aa75a0
fmansi <flavio.mansi@gmail.com>
2018-05-23 12:03:32 -0300

    Melhoria para desmpenho

--- config/copy.config.js
+++ config/copy.config.js
@@ -1,5 +1,9 @@
 // New copy task for font files
 module.exports = {
+  copyAssets: {
+    src: ['{{SRC}}/assets/**/*'],
+    dest: '{{WWW}}/assets'
+  },
   copyFontAwesome: {
     src: ['{{ROOT}}/node_modules/font-awesome/fonts/**/*'],
     dest: '{{WWW}}/assets/fonts'
--- src/components/custom-components.module.ts
+++ src/components/custom-components.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { IonicModule } from 'ionic-angular';
 import { NoItemsComponent } from './no-items/no-items.component';
 import { FaIconComponent } from './fa-icon/fa-icon.component';
+import { TilesComponent } from './tiles/tiles.component';
 
 
 @NgModule({
 	imports: [IonicModule],
-	declarations: [ NoItemsComponent, FaIconComponent],
-	exports: [ NoItemsComponent, FaIconComponent]
+	declarations: [NoItemsComponent, FaIconComponent, TilesComponent],
+	exports: [NoItemsComponent, FaIconComponent, TilesComponent]
 })
 export class CustomComponentsModule {
 }
